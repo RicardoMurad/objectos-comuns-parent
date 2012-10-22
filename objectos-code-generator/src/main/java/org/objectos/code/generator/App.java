4dd8dd073e38f381ec587015513fb55602acaaeb
Ricardo Murad <ricardo.murad@objectos.com.br>
2012-10-22 14:25:27 -0200

    first commit

---
+++ objectos-code-generator/src/main/java/org/objectos/code/generator/App.java
@@ -0,0 +1,13 @@
+package org.objectos.code.generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
